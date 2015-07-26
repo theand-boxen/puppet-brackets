97c512d1ee978efcda8ecdcf2f1a53b109183db4
Heesang Chae <theand@daumcorp.com>
2015-07-26 17:55:46 +0900

    update to v1.3

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # Install Brackets
-class brackets ($version='1.0') {
+class brackets ($version='1.3') {
   package { "brackets-release-${version}-OSX":
     provider => 'appdmg',
-    source   => "https://github.com/adobe/brackets/releases/download/release-${version}/Brackets.Release.${version}.dmg",
+    source   => "https://github.com/adobe/brackets/releases/download/release-1.3%2Beb4/Brackets.1.3.Extract.dmg",
   }
 }
--- spec/classes/brackets-spec.rb
+++ spec/classes/brackets-spec.rb
@@ -6,9 +6,9 @@ describe 'brackets' do
     let (:params) { { } }
 
     it { should contain_class('brackets') }
-    
-    it { should contain_package('Brackets Release 1.0').with_provider('appdmg') }
-    it { should contain_package('Brackets Release 1.0').with_source('https://github.com/adobe/brackets/releases/download/release-1.0/Brackets.Release.1.0.dmg') }
+
+    it { should contain_package('Brackets Release 1.3').with_provider('appdmg') }
+    it { should contain_package('Brackets Release 1.3').with_source('https://github.com/adobe/brackets/releases/download/release-1.3%2Beb4/Brackets.1.3.Extract.dmg') }
   end
 
 end
